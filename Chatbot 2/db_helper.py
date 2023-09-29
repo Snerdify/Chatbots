40c0859635b7d8f08bcd5acde420acee076b058b
Snerdify <srjbeliever0129@gmail.com>
2023-09-29 19:32:20 +0530

    Added functionality for inserting items in the database

--- Chatbot 2/db_helper.py
+++ Chatbot 2/db_helper.py
@@ -5,15 +5,46 @@ global cnx
 # here the cnx is made global so that it can be accessed by all the functions in the script
 # this allows multiple functions to use the same database connection
 
-cnx = mysql.connector.connect (
+cnx = mysql.connector.connect(
     host="localhost",
-    user = "root",
-    password = "root"
-    database = "pandeji_eatery" ) 
+    user="root",
+    password="root",
+    database="pandeyji_eatery"
+)
 
 
-def insert_order_item():
-    return ""
+# function that handles adding an order item
+def insert_order_item(food_item, quantity, order_id):
+    # cursor is a database object that is used to interact with the database
+    # it is used to execute SQL queries , fetch query results , manage transactions
+    try:
+        cursor = cnx.cursor()
+# a "stored-procedure" is a pre-defined database routine
+# here we use callproc named insert_order_item by using sql cursor
+# the stored procedure takes three items, and stores them as a tuple
+        cursor.callproc("insert_order_item",(food_item,quantity,order_id))
+# after inserting an item , we commit the changes in the databse to ensure that 
+# the changes are permanently saved
+        cursor.commit()
+# its important to close the cursor after the operation is complete
+        cursor.close()
+        print("Item successfully inserted")
+        return 1
+    
+#  If an error of type sql.connector.Error occurs , below code executed. 
+# we print an error msg whilst giving the info about what the msg is , 
+#and rollback the changes
+    except mysql.connector.Error as err:
+        print(f"Error inserting an item: {err}")
+
+        # if any changes have been made that are incorrect
+        cnx.rollback()
+        return -1
+# if any other changes other than mysql.connector.Error occur , then below code is executed
+    except Exception as e:
+        print(f"An error occured:{e}")
+        cnx.rollback()
+        return -1
 
 
 def insert_order_tracking():
@@ -28,6 +59,6 @@ def get_next_order_id():
 def get_order_status():
     return ""
 
-if __name__ = "__main__" : 
+if __name__ == "__main__" : 
 
     print(get_next_order_id())
